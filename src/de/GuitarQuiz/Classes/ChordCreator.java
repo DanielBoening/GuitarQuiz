ccf3819593ee0196d47c002f5bb8fe2c34704b60
Junus Ergin <junus91@googlemail.com>
2014-04-28 18:18:21 +0200

    Small Edits

--- src/de/GuitarQuiz/Classes/ChordCreator.java
+++ src/de/GuitarQuiz/Classes/ChordCreator.java
@@ -1,15 +1,21 @@
 package de.GuitarQuiz.Classes;
 
 import android.app.Activity;
-import android.content.Context;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.MusikMonksSolution.guitarquiz.R;
 
 public class ChordCreator {
+	static int ANZAHL = 5; // Anzahl der Spalten
+	static int STRINGS = 6;
 	Chord chord;
 	Activity activity;
+	Tuple IMG_START;
+	Tuple IMG_END;
+	static Tuple BORDER_X = new Tuple(65,20); //Border Links und Rechts, wo kein Bild platziert werden darf
+	static Tuple BORDER_Y = new Tuple(10,20); //Border Oben und Unten, wo kein Bild platziert werden darf
+
 	
 	
 	public ChordCreator(Activity context) {
@@ -22,12 +28,44 @@ public class ChordCreator {
 		this.chord = c;
 		ImageView chordPattern = (ImageView) activity.findViewById(R.id.ChordPattern);
 		RelativeLayout.LayoutParams chordParam = (RelativeLayout.LayoutParams) chordPattern.getLayoutParams();
-		Tuple chordStartPoint = new Tuple(chordParam.leftMargin,chordParam.topMargin);
-		Tuple chordEndPoint = new Tuple(chordParam.leftMargin+chordParam.width,chordParam.topMargin+chordParam.height);
-		
+		IMG_START = new Tuple(chordParam.leftMargin,chordParam.topMargin);
+		IMG_END = new Tuple(chordParam.leftMargin+chordParam.width,chordParam.topMargin+chordParam.height);
+
 		ImageView finger1 = (ImageView) activity.findViewById(R.id.quiz_finger1);
 		RelativeLayout.LayoutParams finger1Params= (RelativeLayout.LayoutParams) finger1.getLayoutParams();
-		finger1Params.setMargins(200, 30, 50, 50);
+		Tuple finger1Position = getImagePosition(13);
+		finger1Params.setMargins(finger1Position.getX(), finger1Position.getY(), 0, 0);
+		activity.findViewById(R.id)
 		
+	}
+	
+	
+	public Tuple getImagePosition(int number){
+		
+		return new Tuple(getXPosition(number), getYPosition(number));
+	}
+	
+	
+	/**
+	 * Returns the current X-Coordinate for any Position depending on the current Image Scale
+	 * @param x Position
+	 * @return
+	 */
+	public int getXPosition(int x){
+		x = x % ANZAHL; // g(1) = 1, g(6) =1, g(7) = 2, ...
+		if(x==0) x = 5;
+		int delta = (IMG_END.getX() - IMG_START.getX() - BORDER_X.getSum()) / (ANZAHL-1); //Abstand
+		return BORDER_X.getX() + (x-1) * delta;	
+	}
+	
+	/**
+	 * Returns the current Y-Coordinate for any Position depending on the current Image Scale
+	 * @param y Position
+	 * @return
+	 */
+	public int getYPosition(int y){
+		y = (int) Math.ceil((double) y / ANZAHL); // f(1) = 1, f(6) = 2, ...
+		int delta = (IMG_END.getY() - IMG_START.getY() - BORDER_Y.getSum()) / (STRINGS-1); //Abstand
+		return BORDER_Y.getX() + (y-1) * delta;	
 	}
 }
