0368ec4a0318e0437c7c2cef5aba88c55c8c05e3
Maximilian Obpacher <maximilian.obpacher@gmail.com>
2014-05-13 19:28:23 +0200

    Reversed useless merge Stuff
    merge stuff deleted

--- src/de/GuitarQuiz/Activities/StartMenu.java
+++ src/de/GuitarQuiz/Activities/StartMenu.java
@@ -1,25 +1,18 @@
 package de.GuitarQuiz.Activities;
 
-<<<<<<< HEAD
 import com.MusikMonksSolution.guitarquiz.R;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-=======
-import android.app.Activity;
-import android.os.Bundle;
->>>>>>> 1658d75f338137583a6f16cac706533ac307b0b6
 
 public class StartMenu extends Activity {
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-<<<<<<< HEAD
 	
 		super.onCreate(savedInstanceState);
 
@@ -33,11 +26,6 @@ public class StartMenu extends Activity {
 	public void play(View v) {
 		Intent myIntent = new Intent(StartMenu.this, Quiz.class);
 		StartMenu.this.startActivity(myIntent);
-=======
-	    super.onCreate(savedInstanceState);
-	
-	    // TODO Auto-generated method stub
->>>>>>> 1658d75f338137583a6f16cac706533ac307b0b6
 	}
 
 }
