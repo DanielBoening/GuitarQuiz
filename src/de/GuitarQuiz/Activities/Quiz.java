c5a24f7027071c488ef9de99faa32146eb1fa135
Junus Ergin <junus91@googlemail.com>
2014-05-13 17:43:34 +0200

    Bitte geht das!

--- src/de/GuitarQuiz/Activities/Quiz.java
+++ src/de/GuitarQuiz/Activities/Quiz.java
@@ -3,33 +3,29 @@ package de.GuitarQuiz.Activities;
 import java.util.ArrayList;
 import java.util.Random;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.MusikMonksSolution.guitarquiz.R;
 
 import de.GuitarQuiz.Classes.Chord;
 import de.GuitarQuiz.Classes.ChordCreator;
-import de.GuitarQuiz.Classes.Helper;
-import de.GuitarQuiz.Classes.Tuple;
-import android.app.Activity;
-import android.os.Bundle;
-import android.os.SystemClock;
-import android.support.v4.view.ViewPager.LayoutParams;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.View;
-import android.widget.Button;
-import android.view.View;
-import android.view.View.MeasureSpec;
-import android.view.ViewTreeObserver.OnGlobalLayoutListener;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 public class Quiz extends Activity {
 	ArrayList<Chord> chords = new ArrayList<Chord>();
 	private Chord rightChordItem;
 	private Button rightButton;
 	int round = 0;
+	boolean buttonIsGreen = false;
 
 	String[] names = { "D-Moll", "D-Dur", "E", "E-Moll" };
 	int[][] fingers = { { 1, 12, 8, 0, 0 }, { 12, 2, 8, 0, 0 },
@@ -66,19 +62,17 @@ public class Quiz extends Activity {
 
 					@Override
 					public void onGlobalLayout() {
-						
-						
+
 						creator = new ChordCreator(correntActivity);
 						prepareForNextRound();
 						/**
-						// Select Random Chord
-						Chord randomChord = chords.get((new Random())
-								.nextInt(chords.size()));
-						rightChordItem = randomChord;
-						creator.setChord(randomChord);
-						printAnswersToButton(randomChord,
-								(ArrayList<Chord>) chords.clone());
-						**/
+						 * // Select Random Chord Chord randomChord =
+						 * chords.get((new Random()) .nextInt(chords.size()));
+						 * rightChordItem = randomChord;
+						 * creator.setChord(randomChord);
+						 * printAnswersToButton(randomChord, (ArrayList<Chord>)
+						 * chords.clone());
+						 **/
 						chordLayout.getViewTreeObserver()
 								.removeGlobalOnLayoutListener(this);
 
@@ -88,12 +82,62 @@ public class Quiz extends Activity {
 
 	}
 
-	public int printAnswersToButton(Chord right, ArrayList<Chord> others) {
+	Handler rightButtonHandler = new Handler();
+	private Runnable letRightButtonBlink = new Runnable() {
+		public void run() {
+			try {
+				if (!buttonIsGreen) {
+					rightButton.setBackgroundResource(R.drawable.correctanswer);
+					buttonIsGreen = true;
+				} else {
+					rightButton.setBackgroundResource(R.drawable.answerbutton);
+					buttonIsGreen = false;
+				}
 
+				// Toast.makeText(getApplicationContext(), ""+buttonIsGreen,
+				// Toast.LENGTH_LONG).show();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	};
+
+	Handler rightButtonGreyHandler = new Handler();
+	private Runnable changeRightButtonGrey = new Runnable() {
+		public void run() {
+			try {
+				rightButton.setBackgroundResource(R.drawable.answerbutton);
+				buttonIsGreen = false;
+
+				// Toast.makeText(getApplicationContext(), ""+buttonIsGreen,
+				// Toast.LENGTH_LONG).show();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	};
+
+	Handler startNextRoundHandler = new Handler();
+	private Runnable runNextRound = new Runnable() {
+		public void run() {
+			try {
+
+				Toast.makeText(getApplicationContext(),
+						"Nächste Runde startet jetzt", Toast.LENGTH_LONG)
+						.show();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	};
+
+	public int printAnswersToButton(Chord right, ArrayList<Chord> others) {
 
 		ArrayList<Button> buttons = getButtonList();
 		ArrayList<Integer> used = new ArrayList<Integer>();
-
 
 		rightAccord = (new Random()).nextInt(buttons.size());
 
@@ -124,23 +168,23 @@ public class Quiz extends Activity {
 		return rightAccord;
 	}
 
-	public ArrayList<Button> getButtonList(){
+	public ArrayList<Button> getButtonList() {
 		Button button1 = (Button) this.findViewById(R.id.quiz_b1);
 		Button button2 = (Button) this.findViewById(R.id.quiz_b2);
 		Button button3 = (Button) this.findViewById(R.id.quiz_b3);
 		Button button4 = (Button) this.findViewById(R.id.quiz_b4);
-		
+
 		ArrayList<Button> buttons = new ArrayList<Button>();
 
 		buttons.add(button1);
 		buttons.add(button2);
 		buttons.add(button3);
 		buttons.add(button4);
-		
+
 		return buttons;
-		
+
 	}
-	
+
 	public void onClick(View v) {
 		int selectedButton = 0;
 		String outputString = "";
@@ -171,7 +215,7 @@ public class Quiz extends Activity {
 
 		if (selectedButton == rightAccord) {
 			outputString = "Richtig!!"; // richtige
-																// Antwort
+										// Antwort
 			if (v.getId() != R.id.nextRound) {
 				Button correctButton = (Button) findViewById(v.getId());
 				correctButton.setBackgroundResource(R.drawable.correctanswer);
@@ -182,52 +226,59 @@ public class Quiz extends Activity {
 			if (v.getId() != R.id.nextRound) {
 				Button falseButton = (Button) findViewById(v.getId());
 				falseButton.setBackgroundResource(R.drawable.falseanswer);
-				rightButton.setBackgroundResource(R.drawable.correctanswer);
-				
-//				try {
-//					for (int i = 0; i < 3; i++) {
-//						rightButton.setBackgroundResource(R.drawable.correctanswer);
-//					//	SystemClock.sleep(250);
-////						rightButton.setBackgroundResource(R.drawable.answerbutton);
-//					//	SystemClock.sleep(250);
-//					}
-//				} catch (Exception E) {
-//					E.printStackTrace();
-//				}
+				int startBlinking = 500;
+				int blinkingDuration = 250;
+				for (int i = 0; i < 5; i++) {
+					rightButtonHandler.postDelayed(letRightButtonBlink,
+							startBlinking + blinkingDuration * i);
+				}
+				// rightButtonHandler.postDelayed(letRightButtonBlink, 900);
+				// rightButtonHandler.postDelayed(letRightButtonBlink, 1100);
+				// rightButtonHandler.postDelayed(letRightButtonBlink, 1300);
+				// rightButtonGreyHandler.postDelayed(changeRightButtonGrey,
+				// 900);
+
+				// try {
+				// for (int i = 0; i < 3; i++) {
+				// rightButton.setBackgroundResource(R.drawable.correctanswer);
+				// // SystemClock.sleep(250);
+				// //
+				// rightButton.setBackgroundResource(R.drawable.answerbutton);
+				// // SystemClock.sleep(250);
+				// }
+				// } catch (Exception E) {
+				// E.printStackTrace();
+				// }
 			}
 		}
-		
+
 		if (nextround) {
-			outputString = "Runde "+round;
+			outputString = "Runde " + round;
 		}
 		t.setText(outputString);
 	}
 
 	private void prepareForNextRound() {
-		
-//		try {
-//			Thread.sleep(1500);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
+
+		// try {
+		// Thread.sleep(1500);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
 		round++;
 		ArrayList<Button> buttons = getButtonList();
 
-		for(Button b:buttons){
+		for (Button b : buttons) {
 			b.setBackgroundResource(R.drawable.answerbutton);
 		}
-		
-		Chord randomChord = chords
-				.get((new Random()).nextInt(chords.size()));
+
+		Chord randomChord = chords.get((new Random()).nextInt(chords.size()));
 		rightChordItem = randomChord;
 		creator.setChord(randomChord);
 		printAnswersToButton(randomChord, (ArrayList<Chord>) chords.clone());
-		
-		
-		
-		
+
 	}
 
 	protected void creatChords() {
