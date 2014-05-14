66e5fa83c83beb4309c5c550059782893f6e1580
Junus Ergin <junus91@googlemail.com>
2014-05-14 10:00:56 +0200

    Added ProgressBar

--- src/de/GuitarQuiz/Activities/StartMenu.java
+++ src/de/GuitarQuiz/Activities/StartMenu.java
@@ -1,15 +1,22 @@
 package de.GuitarQuiz.Activities;
 
-import com.MusikMonksSolution.guitarquiz.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.MusikMonksSolution.guitarquiz.R;
+ 
 
 public class StartMenu extends Activity {
-
+	private ProgressBar progressBar;
+	
+	
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -21,8 +28,20 @@ public class StartMenu extends Activity {
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		setContentView(R.layout.startpage);
+		
+		progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+		progressBar.setMax(100);
+		int progress = 40;
+		setProgressBar(progress);
+		
 	}
 	
+	private void setProgressBar(int progress) {
+		progressBar.setProgress(progress);
+		TextView progressView = (TextView) findViewById(R.id.ProgressView);
+		progressView.setText("Fortschritt: "+progress+"%");		
+	}
+
 	public void play(View v) {
 		Intent myIntent = new Intent(StartMenu.this, Quiz.class);
 		StartMenu.this.startActivity(myIntent);
