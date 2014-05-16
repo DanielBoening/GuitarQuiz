4437eb2248e8bc6c78517de675566ca36c297bdf
Junus Ergin <junus91@googlemail.com>
2014-05-16 14:21:46 +0200

    Level 2 added. Getter & Setter implemented.

--- src/de/GuitarQuiz/Classes/ChordLibrary.java
+++ src/de/GuitarQuiz/Classes/ChordLibrary.java
@@ -10,7 +10,7 @@ public class ChordLibrary {
 	
 	/* ########## Level 1 ########## */
 	
-	private String[] namesLevel1 = { 
+	private static String[] namesLevel1 = { 
 			"D-Moll", 
 			"D-Dur", 
 			"E", 
@@ -20,7 +20,7 @@ public class ChordLibrary {
 			"C", 
 			"A-Moll" 
 			};
-	private int[][] fingersLevel1 = { 
+	private static int[][] fingersLevel1 = { 
 			{ 1, 12, 8, 0, 0 }, 	//D-Moll
 			{ 12, 2, 8, 0, 0 },		//D-Dur
 			{ 11, 22, 17, 0, 0 }, 	//E
@@ -30,7 +30,7 @@ public class ChordLibrary {
 			{ 6, 17, 23, 0, 0 }, 	//C
 			{ 6, 12, 17, 0, 0 } 	//A-Moll
 			};
-	private int[][] isPlayedLevel1 = { 
+	private static int[][] isPlayedLevel1 = { 
 			{ 0, 0, 0, 2, 1, 1 }, 	//D-Moll
 			{ 0, 0, 0, 2, 1, 1 },	//D-Dur
 			{ 2, 2, 0, 0, 0, 2 }, 	//E
@@ -42,5 +42,74 @@ public class ChordLibrary {
 			};
 	
 	
+	/* ########## Level 2 ########## */
+	
+	private static String[] namesLevel2 = { 
+			"G 7", 
+			"C 7", 
+			"B 7", 
+			"A 7", 
+			"D 7", 
+			"E 7", 
+			};
+	private static int[][] fingersLevel2 = { 
+			{ 1, 22, 28, 0, 0 }, 	// G7
+			{ 6, 17, 23, 13, 0 }, 	// C7
+			{ 16, 22, 12, 2, 0 }, 	// B7
+			{ 17, 0, 7, 0, 0 }, 	// A7
+			{ 6, 12, 2, 0, 0 }, 	// D7
+			{ 11, 22, 0, 0, 0 }, 	// E7
+			};
+	private static int[][] isPlayedLevel2 = { 
+			{ 0, 2, 2, 2, 0, 0 }, 	// G7 
+			{ 2, 0, 0, 0, 0, 1 }, 	// C7
+			{ 0, 2, 0, 0, 0, 1 }, 	// B7
+			{ 2, 0, 2, 0, 2, 1 }, 	// A7
+			{ 0, 0, 0, 2, 1, 1 }, 	// D7
+			{ 2, 2, 0, 2, 0, 2 }, 	// E7
+
+			};
+	
+	
+	
+	
+	
+	
+	
+	public static String[] getNames(int i){
+		switch (i) {
+			case 1:
+				return namesLevel1;
+			case 2:
+				return namesLevel2;
+			default:
+				// TODO
+		}
+		return null;
+	}
+	
+	public static int[][] getFingersLevel(int i){
+		switch (i) {
+			case 1:
+				return fingersLevel1;
+			case 2:
+				return fingersLevel2;
+			default:
+				// TODO
+		}
+		return null;
+	}
+	
+	public static int[][] getIsPlayedLevel(int i){
+		switch (i) {
+			case 1:
+				return isPlayedLevel1;
+			case 2:
+				return isPlayedLevel2;
+			default:
+				// TODO
+		}
+		return null;
+	}
 	
 }
