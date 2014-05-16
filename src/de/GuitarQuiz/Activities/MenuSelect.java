8ed3e1a1ee9170460b5c0cb331d6ddf8e9423d01
Junus Ergin <junus91@googlemail.com>
2014-05-17 00:53:20 +0200

    Alert beim Löschen der Datenbank

--- src/de/GuitarQuiz/Activities/MenuSelect.java
+++ src/de/GuitarQuiz/Activities/MenuSelect.java
@@ -4,6 +4,9 @@ import com.MusikMonksSolution.guitarquiz.R;
 
 import de.GuitarQuiz.Classes.UserDataBase;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +14,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
-public class MenuSelect extends Activity {
+public class MenuSelect extends Activity implements OnClickListener {
 	private UserDataBase userDataBase = new UserDataBase();
 	TextView level1View;
 	TextView level2View;
@@ -28,28 +31,33 @@ public class MenuSelect extends Activity {
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		setContentView(R.layout.menuselect);
-		
+
 		userDataBase.load(this);
-		level1View = (TextView) findViewById(R.id.level1TextView);		
-		level2View = (TextView) findViewById(R.id.level2TextView);		
-		
+		level1View = (TextView) findViewById(R.id.level1TextView);
+		level2View = (TextView) findViewById(R.id.level2TextView);
+
 	}
-	
-	
+
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
-		level1View.setText("HighScore: "+userDataBase.getHighScore(1));
-		level2View.setText("HighScore: "+userDataBase.getHighScore(2));
+		level1View.setText("HighScore: " + userDataBase.getHighScore(1));
+		level2View.setText("HighScore: " + userDataBase.getHighScore(2));
 
 	}
 
+	public void resetDatabase(View v) {
+		AlertDialog ad = new AlertDialog.Builder(this)
+				.setMessage(
+						"Achtung.\nSollen deine Daten wirklich unwiderruflich gelöscht werden?")
+				.setIcon(R.drawable.ic_launcher).setTitle("Daten löschen")
+				.setPositiveButton("Ja", this).setNegativeButton("Nein", this)
+				.setCancelable(false).create();
+		ad.show();
+		;
 
-	public void resetDatabase(View v){
-		userDataBase.resetDataBase();
-		level1View.setText("HighScore: "+userDataBase.getHighScore(1));
-		level2View.setText("HighScore: "+userDataBase.getHighScore(2));
+
 
 	}
 
@@ -72,6 +80,28 @@ public class MenuSelect extends Activity {
 		Quiz.setLevel(level);
 		Intent myIntent = new Intent(MenuSelect.this, Quiz.class);
 		MenuSelect.this.startActivity(myIntent);
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		// TODO Auto-generated method stub
+		switch (which) {
+		case DialogInterface.BUTTON_POSITIVE: // yes
+			userDataBase.resetDataBase();
+			level1View.setText("HighScore: " + userDataBase.getHighScore(1));
+			level2View.setText("HighScore: " + userDataBase.getHighScore(2));
+			
+			
+			break;
+		case DialogInterface.BUTTON_NEGATIVE: // no
+
+			break;
+		case DialogInterface.BUTTON_NEUTRAL: // neutral
+			break;
+		default:
+			// nothing
+			break;
+		}
 	}
 
 }
